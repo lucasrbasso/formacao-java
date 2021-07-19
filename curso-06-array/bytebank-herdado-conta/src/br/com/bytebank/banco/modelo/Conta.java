6477586a61ed9e91edec4d00839cdb8718b52710
Lucas Basso <lucas.rbasso@outlook.com>
2021-07-19 10:54:59 -0300

    linkedList

--- curso-06-array/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ curso-06-array/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -91,7 +91,18 @@ public abstract class Conta extends Object {
     public static int getTotal(){
         return Conta.total;
     }
-    
+
+    @Override
+    public boolean equals(Object ref) {
+        Conta outra = (Conta) ref;
+
+        if(this.agencia != outra.agencia) {
+            return false;
+        }
+
+        return this.numero == outra.numero;
+    }
+
     @Override
 	public String toString() {
 		return "Numero: " + this.numero + ", Agencia: " + this.agencia;
