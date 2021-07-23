57628f40dece024a9eea7b40b947e63d7b385771
Lucas Basso <lucas.rbasso@outlook.com>
2021-07-23 11:44:30 -0300

    java-8

---
+++ curso-09-java-8/src/Streams.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+record Curso(String nome, int alunos) {
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAlunos() {
+        return alunos;
+    }
+}
+
+public class Streams {
+    public static void main(String[] args) {
+        List<Curso> cursos = new ArrayList<Curso>();
+        cursos.add(new Curso("Python", 45));
+        cursos.add(new Curso("JavaScript", 150));
+        cursos.add(new Curso("Java 8", 113));
+        cursos.add(new Curso("C", 55));
+
+        cursos.sort(Comparator.comparing(Curso::getAlunos));
+
+        int sum = cursos.stream()
+                .filter(c -> c.getAlunos() >= 100)
+                .mapToInt(Curso::getAlunos)
+                .sum();
+        System.out.println(sum);
+
+        cursos.stream()
+                .filter(c -> c.getAlunos() >= 100).findAny()
+                .ifPresent(curso -> System.out.println(curso.getNome()));
+
+        cursos = cursos.stream()
+                .filter(c -> c.getAlunos() >= 100)
+                .collect(Collectors.toList());
+
+        cursos.stream().forEach(c -> System.out.println(c.getNome()));
+        cursos.forEach(c -> System.out.println(c.getNome()));
+
+        cursos.stream()
+                .filter(c -> c.getAlunos() >= 100)
+                .collect(Collectors.toMap(
+                        Curso::getNome,
+                        Curso::getAlunos
+                ))
+                .forEach((nome, alunos) -> System.out.println(nome + " tem " + alunos + " alunos"));
+    }
+}
